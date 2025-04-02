2aa1ddef80ec3b3f5ff8a1029bf81375bfbb7bae
notfork-h <hacene@unforkable.co>
2025-04-02 15:03:22 +0200

    feat: Add forking configuration to Hardhat for NXChain network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://rpc.nxchainscan.com/",
+        blockNumber: undefined,
+      },
+    },
+  },
 };
-
 export default config;
